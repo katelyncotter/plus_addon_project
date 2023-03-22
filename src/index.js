8248ab289ce45771bc4bd5cf7dbfa8d9dce6d45e
Katelyn Cotter <40216370+katelyncotter@users.noreply.github.com>
2023-03-22 13:22:29 -0400

    updated Boston timezone

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
+function updateTime() {
+  // Boston
+  let bostonElement = document.querySelector("#boston");
+  if (bostonElement) {
+    let bostonDateElement = bostonElement.querySelector(".date");
+    let bostonTimeElement = bostonElement.querySelector(".time");
+    let bostonTime = moment().tz("America/New_York");
 
+    bostonDateElement.innerHTML = bostonTime.format("MMMM	Do YYYY");
+    bostonTimeElement.innerHTML = bostonTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+}
+
+updateTime();
+setInterval(updateTime, 1000);
